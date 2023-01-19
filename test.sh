a57a8f38bf5247046c57611b624b9e4f2e3b810b
James Nock <jpnock@gmail.com>
2023-01-19 23:10:59 +0000

    test: Don't use compressed code

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 OUT="bin/test1"
 
 mkdir -p bin
-/opt/riscv/bin/riscv64-unknown-elf-gcc -march=rv32imfdc -mabi=ilp32d -o "${OUT}" test/main.c
+/opt/riscv/bin/riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -o "${OUT}" test/main.c
 
 set +e
 spike pk "${OUT}"
